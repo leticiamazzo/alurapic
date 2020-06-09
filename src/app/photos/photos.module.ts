9fda0cc3cc7e71f5b1feb0db23b1bfa763827ed8
leticia mazzo <leticia.mazzo@bgmrodotec.com.br>
2020-06-08 21:28:30 -0300

    Added photos mock

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhotoComponent } from './photo/photo.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    PhotoComponent
+  ],
+  declarations: []
+})
+export class PhotosModule { }
