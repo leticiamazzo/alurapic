4bdd8262acf0568a407d433176ba4de20fba84b9
leticia mazzo <leticia.mazzo@bgmrodotec.com.br>
2020-06-14 12:23:30 -0300

    consumed api

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HttpClientModule } from '@angular/common/http';
 import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
+  declarations: [ PhotoComponent ],
+
+  exports: [ PhotoComponent ],
+
   imports: [
     CommonModule,
-    PhotoComponent
-  ],
-  declarations: []
+    HttpClientModule,
+  ]
 })
 export class PhotosModule { }
